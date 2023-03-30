e319ed39a0714eaed073cc1e30f0a3b5f84b8d47
Pratyush Kumar <Pratyush Kumar@SMTC-R-0172>
2023-03-30 11:10:18 +0530

    first python project

---
+++ python_project/python_programe/print2.py
@@ -0,0 +1,6 @@
+'''
+Created on 30-Mar-2023
+
+@author: Pratyush Kumar
+'''
+print("hello world")
---
+++ python_project/python_programe/printing.py
@@ -0,0 +1,7 @@
+'''
+Created on 30-Mar-2023
+
+@author: Pratyush Kumar
+'''
+s1="hello pratyush"
+print(s1)
